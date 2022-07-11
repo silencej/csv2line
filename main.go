d9b1d54ec9d9d4227a5fc85d7dd7887b8349d801
silencej <owen263@gmail.com>
2022-07-11 19:11:05 +0800

    Init

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	//  "errors"
+	//  "flag"
+	"fmt"
+	// "io"
+	//  "io/ioutil"
+	"log"
+	"os"
+	//  "time"
+	"bufio"
+	"encoding/csv"
+	"github.com/urfave/cli/v2"
+	"strings"
+)
+
+var TAGS = "CSV2LINE_TAGS"
+var FIELDS = "CSV2LINE_FIELDS"
+
+//----------
+
+func readCsv(inputFile string) (output []map[string]string, err error) {
+
+	f, err := os.Open(inputFile)
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//----- Read
+
+	var b strings.Builder
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		t := scanner.Text()
+
+		// Remove the comment lines.
+		if len(t) > 0 && t[0] != '#' {
+			b.WriteString(t)
+			b.WriteString("\n")
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading error:", err)
+	}
+
+	r := csv.NewReader(strings.NewReader(b.String()))
+	records, err2 := r.ReadAll()
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	//----- Format
+
+	output = []map[string]string{}
+	header := records[0]
+	for i := 1; i < len(records); i++ {
+		dict := map[string]string{}
+		for j := range header {
+			dict[header[j]] = records[i][j]
+		}
+		output = append(output, dict)
+	}
+	return output, nil
+}
+
+func writeCsv(outputFile string, records []map[string]string) {
+	f, err := os.OpenFile(outputFile, os.O_RDWR|os.O_CREATE, 0644)
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var tags = strings.Split(os.ExpandEnv("${"+TAGS+"}"), ",")
+	fmt.Printf("%q: %q\n", TAGS, tags)
+	var fields = strings.Split(os.ExpandEnv("${"+FIELDS+"}"), ",")
+	fmt.Printf("%q: %q\n", FIELDS, fields)
+
+	writer := bufio.NewWriter(f)
+	for i := 0; i < len(records); i++ {
+		writer.WriteString(records[i]["_measurement"])
+		for _, tag := range tags {
+			writer.WriteString(fmt.Sprintf(",%v=\"%v\"", tag, records[i][tag]))
+		}
+		for _, field := range fields {
+			writer.WriteString(fmt.Sprintf(" %v=\"%v\"", field, records[i][field]))
+		}
+		writer.WriteString("\n")
+	}
+}
+
+func main() {
+
+	app := &cli.App{}
+	// app.UseShortOptionHandling = true
+
+	app.Action = func(c *cli.Context) error {
+		input := c.Args().Get(0)
+		output := c.Args().Get(1)
+		fmt.Printf("Input: %q, Output: %q\n", input, output)
+
+		records, _ := readCsv(input)
+		writeCsv(output, records)
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
